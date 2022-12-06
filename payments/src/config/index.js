fa984f9d0393686d81f2694f2c6ff5d2b6ee3c94
geekyabhi <thakurabhinav17122001@gmail.com>
2022-12-06 12:33:16 +0530

    Payment service started

---
+++ payments/src/config/index.js
@@ -0,0 +1,20 @@
+const dotEnv = require("dotenv").config({ path: "./.env" });
+
+// if (process.env.NODE_ENV !== "prod") {
+//   const configFile = `./.env.${process.env.NODE_ENV}`;
+//   // dotEnv.config({ path: configFile });
+// } else {
+//   dotEnv.config();
+// }
+
+module.exports = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.MONGODB_URI,
+  APP_SECRET: process.env.APP_SECRET,
+  EXCHANGE_NAME: "ECOMMERCE",
+  MESSAGE_QUEUE_URL: process.env.RABBIT_MQ_URL,
+  SHOPPING_BINDING_KEY: "SHOPPING_SERVICE",
+  CUSTOMER_BINDING_KEY: "CUSTOMER_SERVICE",
+  PRODUCT_BINDING_KEY: "PRODUCT_SERVICE",
+  QUEUE_NAME: "ECOMMERCE_QUEUE",
+};
---
+++ payments/src/express-app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const { products } = require("./api");
+const ErrorHandler = require("./utils/error/error-handler");
+const morganMiddleware = require("./middlewares/morgan");
+
+module.exports = async (app, channel) => {
+	app.use(morganMiddleware);
+	app.use(express.json({ limit: "1mb" }));
+	app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+	app.use(cors());
+	app.use(express.static(__dirname + "/public"));
+
+	app.use("/status", (req, res, next) => {
+		res.send("Products service running properly");
+	});
+
+	products(app, channel);
+
+	app.use(ErrorHandler);
+};
